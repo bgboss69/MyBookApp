519a9dd728f2d7ba7169fb3875e390e54b9d35a6
bgboss69 <cky10490507@gmail.com>
2025-06-14 17:40:42 +0800

    initial next

--- book-app/src/app/book/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { ReactNode } from 'react'
-import { BookProvider } from '@/providers/book-context'
-
-export default function BookLayout({ children }: { children: ReactNode }) {
-  return <BookProvider>{children}</BookProvider>
-}
